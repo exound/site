dc34be175831446eb02275ff64a63a5712b5c089
Kaid Wong <kaid@kaid.me>
2015-11-13 18:24:02 +0800

    Added sign_out page

--- src/scripts/components/pages/SignOut.js
+++ src/scripts/components/pages/SignOut.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+import hasTimers from "../../decorators/hasTimers";
+import hasHistory from "../../decorators/hasHistory";
+
+@hasTimers
+@hasHistory
 export default class SignOut extends React.Component {
+  componentDidMount() {
+    this.setTimeout(() => {
+      localStorage.removeItem("authToken");
+      this.history.pushState(null, "/");
+    }, 1600);
+  }
+
   render() {
     return (
-      <main>
+      <main className="session">
+        <div className="form">
+          <h1>正在登出</h1>
+        </div>
       </main>
     );
   }

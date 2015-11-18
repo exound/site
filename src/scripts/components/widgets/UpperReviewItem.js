49f9f7d2424ffaec815c69ff5690eba0889b7c39
Kaid Wong <kaid@kaid.me>
2015-11-18 13:53:59 +0800

    Added pm2 to daemonize production server

--- src/scripts/components/widgets/UpperReviewItem.js
+++ src/scripts/components/widgets/UpperReviewItem.js
@@ -9,48 +9,22 @@ export default class UpperReviewItem extends React.Component {
   };
 
   render() {
-
     const {
       review,
-      index,
       active
     } = this.props;
 
     const {
       id,
       title,
-      review_meta: {
-        price,
-        scores
-      }
+      cover
     } = review;
 
-    const values = R.values(scores)
-        , total = R.reduce(
-          R.add,
-          0,
-          R.map(value => value.score, values)
-        )
-        , average = parseFloat(total ? total / values.length : 0);
+    const className = active ? "review active" : "review";
 
-    const className = active ?
-          "review active" :
-          "review";
-
-    const scoreDisplay = average &&
-          <span className="score">评分: {average}</span>;
-
-    const priceDisplay = price &&
-          <span className="price">价格: {price}</span>;
-          
     return (
       <div onMouseOver={this.hover} className={className}>
         <h2><Link to={`/articles/${id}`}>{title}</Link></h2>
-
-        <div className="meta">
-          {scoreDisplay}
-          {priceDisplay}
-        </div>
       </div>
     );
   }
